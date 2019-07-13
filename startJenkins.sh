38b7981d428635fa504492e668719caa17161365
Sam <sg@samga.in>
2019-07-13 18:05:11 +0100

    .

--- startJenkins.sh
+++ startJenkins.sh
@@ -1,2 +1,2 @@
 docker build -t jenkins .
-docker run -d -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home jenkins
+docker run -d -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v jenkins_home:/var/jenkins_home jenkins
